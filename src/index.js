b5c337f0555f4b98ce5cfb0e78d64ef3441e28f2
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2023-03-04 20:15:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { ConfigurationOptions } from 'aws-sdk'
 
 const configuration={
     region: 'ap-south-1',
-    secretAccessKey: 'y0Rs83WvttQMS21pcbH/TO3PkiDubOB+i5lxAhMh',
-    accessKeyId: 'AKIAVUZGXF7VTG3TDZO7'
+    secretAccessKey: '',
+    accessKeyId: ''
 }
 
 AWS.config.update(configuration)
